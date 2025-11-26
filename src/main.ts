be9747083d471bdd346a7b0eba8d9eaabd3cfe50
rchcu9-dotcom <rchcu9@gmail.com>
2025-11-27 00:28:00 +0100

    fix: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "https://table-de-marque-72e86.web.app",
+      "https://table-de-marque-72e86.firebaseapp.com"
+    ],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
